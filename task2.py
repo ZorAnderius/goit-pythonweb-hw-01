e99d94d9afa167d3e210c2549cff74f21bcf364d
ZorAnderius <gabriel.zorander@gmail.com>
2024-10-31 10:40:07 +0000

    add logging and black formatting

--- task2.py
+++ task2.py
@@ -1,8 +1,19 @@
+import logging
 from abc import ABC, abstractmethod
+
+logging.basicConfig(
+    format="%(asctime)s = %(name)s - %(levelname)s: %(message)s",
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler("task2.log", encoding="utf-8"),
+        logging.StreamHandler(),
+    ],
+)
+logger = logging.getLogger("Task2")
 
 
 class Book:
-    def __init__(self, title: str, author: str, year: str):
+    def __init__(self, title: str, author: str, year: str) -> None:
         self.title = title
         self.author = author
         self.year = year
@@ -13,53 +24,60 @@ class Book:
 
 class LibraryInterface(ABC):
     @abstractmethod
-    def add_book(self, book: Book):
+    def add_book(self, book: Book) -> None:
         pass
 
     @abstractmethod
-    def remove_book(self, title: str):
+    def remove_book(self, title: str) -> None:
         pass
 
     @abstractmethod
-    def show_books(self):
+    def show_books(self) -> None:
         pass
 
 
 class Library(LibraryInterface):
-    def __init__(self):
+    def __init__(self) -> None:
         self.books = []
 
-    def add_book(self, book: Book):
+    def add_book(self, book: Book) -> None:
         self.books.append(book)
+        logger.info(f'Book "{book.title}" added successful!')
 
-    def remove_book(self, title: str):
+    def remove_book(self, title: str) -> None:
         for book in self.books:
             if book.title == title:
                 self.books.remove(book)
+                logger.info(f'Book "{book.title}" removed successful!')
                 break
+        else:
+            logger.info(f'Book "{title}" not found')
 
-    def show_books(self):
-        [print(book) for book in self.books]
+    def show_books(self) -> None:
+        if self.books:
+            [logger.info(book) for book in self.books]
+        else:
+            logger.info('Library is empty.')
 
 
 class LibraryManager:
-    def __init__(self, library: Library):
+    def __init__(self, library: Library) -> None:
         self.library = library
 
-    def add_book(self, title: str, author: str, year: str):
+    def add_book(self, title: str, author: str, year: str) -> None:
         book = Book(title, author, year)
         self.library.add_book(book)
 
-    def remove_book(self, title):
+    def remove_book(self, title) -> None:
         self.library.remove_book(title)
 
-    def show_books(self):
+    def show_books(self) -> None:
         self.library.show_books()
 
 
 def main():
-    library = Library()
-    manager = LibraryManager(library)
+    library: Library = Library()
+    manager: LibraryManager = LibraryManager(library)
     while True:
         command = input("Enter command (add, remove, show, exit): ").strip().lower()
         match command:
